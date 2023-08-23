086fc011d0c9064ee8019b2257b345a2dafd17fc
Paulo Salvatore <salvatoregames@gmail.com>
2023-08-23 11:44:42 -0300

    Implement first activity (basic behavior)

--- app/src/main/java/com/salvatoreacademy/sa_dev_android_nativo_nivel02_fase02/MainActivity.kt
+++ app/src/main/java/com/salvatoreacademy/sa_dev_android_nativo_nivel02_fase02/MainActivity.kt
@@ -2,10 +2,27 @@ package com.salvatoreacademy.sa_dev_android_nativo_nivel02_fase02
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // Encontrar os elementos de interface
+        // findViewById
+        //  <> -> Tipo da informação (TextView, EditText, Button etc)
+        //  () -> ID da informação (R.id.tvResult etc)
+        // val -> variáveis que não mudam de valor
+        // var -> variáveis que podem mudar de valor
+        val btSend = findViewById<Button>(R.id.btSend)
+        val tvResult = findViewById<TextView>(R.id.tvResult)
+
+        // Declarar um Listener
+        btSend.setOnClickListener {
+            // Tudo que eu escrever dentro das chaves, será executado quando clicar no botão
+            tvResult.text = "Paulo Salvatore"
+        }
     }
 }
