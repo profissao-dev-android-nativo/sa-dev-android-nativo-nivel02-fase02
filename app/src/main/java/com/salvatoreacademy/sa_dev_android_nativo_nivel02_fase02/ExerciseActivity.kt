ad09e476ff58b1a43505047e6249f7cfdb3fbc85
Paulo Salvatore <salvatoregames@gmail.com>
2023-08-24 09:40:07 -0300

    Implement basic behavior

--- app/src/main/java/com/salvatoreacademy/sa_dev_android_nativo_nivel02_fase02/ExerciseActivity.kt
+++ app/src/main/java/com/salvatoreacademy/sa_dev_android_nativo_nivel02_fase02/ExerciseActivity.kt
@@ -2,10 +2,21 @@ package com.salvatoreacademy.sa_dev_android_nativo_nivel02_fase02
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 
 class ExerciseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_exercise)
+
+        val tvResult = findViewById<TextView>(R.id.tvResult)
+        val etEmail = findViewById<TextView>(R.id.etEmail)
+        val btSend = findViewById<TextView>(R.id.btSend)
+
+        btSend.setOnClickListener {
+            val email = etEmail.text.toString()
+
+            tvResult.text = email
+        }
     }
 }
