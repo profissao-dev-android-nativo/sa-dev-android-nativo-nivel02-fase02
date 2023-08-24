d2e7ec522db899f512d27f50472ebf9e960c8608
Paulo Salvatore <salvatoregames@gmail.com>
2023-08-24 09:29:24 -0300

    Create `ExerciseActivity`

---
+++ app/src/main/java/com/salvatoreacademy/sa_dev_android_nativo_nivel02_fase02/ExerciseActivity.kt
@@ -0,0 +1,11 @@
+package com.salvatoreacademy.sa_dev_android_nativo_nivel02_fase02
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ExerciseActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_exercise)
+    }
+}
