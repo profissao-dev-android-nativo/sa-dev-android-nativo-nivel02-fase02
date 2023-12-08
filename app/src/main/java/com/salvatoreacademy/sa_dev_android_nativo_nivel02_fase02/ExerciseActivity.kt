f735d932ccbed4fc45dd2661346947729ccea929
Paulo Salvatore <salvatoregames@gmail.com>
2023-12-08 13:50:59 -0300

    Implement `etName` & `tvName` behavior

--- app/src/main/java/com/salvatoreacademy/sa_dev_android_nativo_nivel02_fase02/ExerciseActivity.kt
+++ app/src/main/java/com/salvatoreacademy/sa_dev_android_nativo_nivel02_fase02/ExerciseActivity.kt
@@ -6,33 +6,51 @@ import android.util.Patterns
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import android.widget.Toast
 
 class ExerciseActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_exercise)
 
-        val tvResult = findViewById<TextView>(R.id.tvEmail)
+        val tvName = findViewById<TextView>(R.id.tvName)
+        val etName = findViewById<EditText>(R.id.etName)
+        val tvEmail = findViewById<TextView>(R.id.tvEmail)
         val etEmail = findViewById<EditText>(R.id.etEmail)
         val btSend = findViewById<Button>(R.id.btSend)
         val btClear = findViewById<Button>(R.id.btClear)
 
         // Button Send Behavior
         btSend.setOnClickListener {
+            val name = etName.text.toString()
             val email = etEmail.text.toString()
+
+            var isValid = true
+
+            if (name.isBlank()) {
+                etName.error = "Insert your name!"
+                isValid = false
+            }
 
             if (email.isBlank()) {
                 etEmail.error = "Insert your e-mail address!"
+                isValid = false
             } else if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
                 etEmail.error = "Please enter a valid e-mail address!"
-            } else {
-                tvResult.text = email
+                isValid = false
+            }
+
+            if (isValid) {
+                tvName.text = name
+                tvEmail.text = email
+
+                Toast.makeText(this, "Inputs updated successfully!", Toast.LENGTH_SHORT).show()
             }
         }
 
         // Button Clear Behavior
         btClear.setOnClickListener {
-            tvResult.text = ""
+            tvEmail.text = ""
             etEmail.text = null
         }
     }
